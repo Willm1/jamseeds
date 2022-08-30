e4aaa737ddaa957c27878af45e530b4212374584
Leith Atia <leith.atia@gmail.com>
2022-08-30 14:02:35 +0100

    created css template show page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,9 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: %i[home css_template]
 
   def home
+  end
+
+  def css_template
   end
 end
