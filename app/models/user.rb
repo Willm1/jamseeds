a4cd5960485d8064f97d38a8b839b3376e206107
Will May <w.p.may@hotmail.co.uk>
2022-08-31 15:51:17 +0100

    updates

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,13 @@ class User < ApplicationRecord
   has_many :instruments, through: :user_instruments
   has_many :genres, through: :user_genres
   has_many :instruments, through: :user_instruments
+
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+  def age
+    ((Time.zone.now - dob.to_time) / 1.year.seconds).floor
+  end
 end
