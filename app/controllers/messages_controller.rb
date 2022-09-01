35ec567bff2243b4ce0274dfb0006b3a2ec8c88f
Leith Atia <leith.atia@gmail.com>
2022-09-01 14:59:49 +0100

    chatroom layout fixed

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,7 +2,7 @@ class MessagesController < ApplicationController
   def create
     @message = current_user.messages.create(content: message_params[:content], chatroom_id: params[:chatroom_id])
   end
-  
+
   private
 
   def message_params
