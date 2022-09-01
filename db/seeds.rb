60dd4cfb623da35b7e834913396609b2bc856795
Pedro Ferreira <pedrom2ferreira@gmail.com>
2022-09-01 01:36:21 +0100

    cards and column additions

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,15 @@ User.destroy_all
 puts "Creating database"
 # generate 30 users
 
+
 30.times do
   User.create!(
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
     email: Faker::Internet.email,
     postcode: Faker::Address.zip_code,
+    city: Faker::Address.city,
+    country: Faker::Address.country,
     dob: Faker::Date.birthday(min_age: 18),
     ability: rand(1..3),
     image_url: Faker::Avatar.image,
