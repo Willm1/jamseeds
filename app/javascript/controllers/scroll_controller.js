1bd98b04852a1e462c79e8bca0e44ccea7adbc4e
Leith Atia <leith.atia@gmail.com>
2022-09-05 14:25:36 +0100

    chatroom scroll function done

--- app/javascript/controllers/scroll_controller.js
+++ app/javascript/controllers/scroll_controller.js
@@ -3,21 +3,19 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="scroll"
 export default class extends Controller {
   connect() {
-    console.log("Connected");
-    const messages = document.getElementById("messages");
-    messages.addEventListener("DOMNodeInserted", this.resetScroll);
-    this.resetScroll(messages);
+    this.element.addEventListener("DOMNodeInserted", this.#resetScroll);
+    this.#resetScroll();
   }
 
   disconnect() {
     console.log("Disconnected");
   }
 
-  resetScroll(messages) {
-    console.log("Trying to scroll");
-    console.log(messages.scrollTop);
-    console.log(messages.scrollHeight);
-    console.log(messages.clientHeight);
+  #resetScroll() {
     // messages.scrollTop = messages.scrollHeight - messages.clientHeight;
+    let temp = document.querySelector(".messages-container");
+
+    console.log("Should be scrolling");
+    temp.scrollTo(0, temp.scrollHeight);
   }
 }
