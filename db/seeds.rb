12091177c590884a9ffadb6acb5ec88a1706113d
Leith Atia <leith.atia@gmail.com>
2022-09-06 03:53:42 +0100

    Revert "fixed scroll bug"
    This reverts commit 601a013f4d953e27dc82a755bc35b733d74eb9cd.
    
    reverting to 601a013

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,6 @@ UserInstrument.destroy_all
 UserGenre.destroy_all
 User.destroy_all
 Instrument.destroy_all
-Chatroom.destroy_all
-ChatroomUser.destroy_all
 
 puts "Creating database"
 # generate 30 users
