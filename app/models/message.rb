120294875f6c124123f7d468eb123605f44d4722
Leith Atia <leith.atia@gmail.com>
2022-09-06 04:08:08 +0100

    fixed scrollbar issue

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,7 @@
 class Message < ApplicationRecord
   belongs_to :user
   belongs_to :chatroom
-  after_create_commit :broadcast_message_to_chatroom
+  after_create_commit { broadcast_append_to chatroom }
 
   before_create :confirm_participant
 
@@ -10,10 +10,5 @@ class Message < ApplicationRecord
     # raise
     is_participant = ChatroomUser.find_by(user_id: user.id, chatroom_id: chatroom.id)
     throw :abort unless is_participant
-  end
-
-  def broadcast_message_to_chatroom
-    return nil unless current_user
-    broadcast_append_to(chatroom, locals: { user: current_user })
   end
 end
