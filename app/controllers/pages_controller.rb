742315fb2adfb9f78b8273624203d54bf86ee815
Will May <w.p.may@hotmail.co.uk>
2022-09-08 14:40:15 +0100

    update

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,13 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: %i[home css_template]
+  skip_before_action :authenticate_user!, only: %i[home error]
 
   def home
     redirect_to users_path if user_signed_in?
     @users = User.all.last(4)
   end
 
-  
+  def error
+  end
+
+
 end
